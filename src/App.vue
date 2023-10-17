fcf345c81fd65846135f2140810e76690d3a7737
almasutami <86611956+almasutami@users.noreply.github.com>
2023-10-17 09:24:48 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="toolbar">Toolbar</div>
+    <div class="container-list">Movie list here</div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.toolbar {
+  height: 50px;
+  background-color: #f5f5f5;
+}
+</style>
